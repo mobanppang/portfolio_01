f927798fe38401e06c37daa802e0489293511098
mobanppang <51999488+mobanppang@users.noreply.github.com>
2019-09-14 15:57:18 +0900

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,10 +1,3 @@
 $(document).ready(function(){
             $('.contents').fullpage({})
         })
-function btn(seq){
-    var box=
-        $('#box'+seq).offset()
-        $('html,body').animate({
-            scrollTop:box.top
-        },)
-    }
---
+++ script/script2.js
@@ -0,0 +1,8 @@
+function btn(seq){
+    var box=
+        $('#box'+seq).offset()
+        $('html,body').animate({
+            scrollTop:box.top
+        },)
+        
+    }
